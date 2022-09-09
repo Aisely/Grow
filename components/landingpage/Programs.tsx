c5d57f84d0656ab818caee3690e141190d7f8bce
Caystone99 <emmanuelcaleb546@gmail.com>
2022-09-09 13:26:14 +0100

    Programs component
    Added the programs component for the course page

---
+++ components/landingpage/Programs.tsx
@@ -0,0 +1,73 @@
+import {useEffect} from 'react'
+
+function Programs(){
+    function handleClick(){
+        
+    }
+    return(
+        <section id="programs">
+            <div className="container mx-auto">
+                <h2 className="mt-32 text-center font-bold text-6xl">
+                    Courses
+                </h2>
+                <div className="flex flex-col mt-10 text-center">
+                    <div className='text-justify px-10 m-4 space-y-5'>
+                        <div className='program-head text-2xl cursor-pointer font-semibold flex flex-row justify-between'>
+                            <h2> Entry-level Developer</h2>
+                            <span>+</span>
+                        </div>
+                        <div className='program-body m-auto p-5 font-medium'>
+                            <p>Android Developer Bootcamp</p>
+                            <p>Web Developer Bootcamp</p>
+                        </div>
+                        <hr className='mx-auto'/>
+                    </div>
+
+                    <div className='text-justify px-10 m-4 space-y-5'>
+                        <div className='program-head text-2xl cursor-pointer font-semibold flex flex-row justify-between'>
+                            <h2> Junior Developer</h2>
+                            <span>+</span>
+                        </div>
+                        <div className='program-body m-auto p-5 font-medium hidden'>
+                            <p>Android Developer Bootcamp</p>
+                            <p>Frontend Developer Bootcamp</p>
+                            <p>Backend Developer Bootcamp</p>
+                            <p>Fullstack Developer Bootcamp</p>
+                        </div>
+                        <hr className='mx-auto'/>
+                    </div>
+
+                    <div className='text-justify px-10 m-4 space-y-5'>
+                        <div className='program-head text-2xl cursor-pointer font-semibold flex flex-row justify-between'>
+                            <h2> Intermediate Developer</h2>
+                            <span>+</span>
+                        </div>
+                        <div className='program-body m-auto p-5 font-medium hidden'>
+                            <p>Android Developer Bootcamp</p>
+                            <p>Frontend Developer Bootcamp</p>
+                            <p>Backend Developer Bootcamp</p>
+                            <p>Fullstack Developer Bootcamp</p>
+                        </div>
+                        <hr className='mx-auto'/>
+                    </div>
+
+                    <div className='text-justify px-10 m-4 space-y-5'>
+                        <div className='program-head text-2xl cursor-pointer font-semibold flex flex-row justify-between'>
+                            <h2> Senior Developer</h2>
+                            <span>+</span>
+                        </div>
+                        <div className='program-body m-auto p-5 font-medium hidden'>
+                            <p>Android Developer Bootcamp</p>
+                            <p>Frontend Developer Bootcamp</p>
+                            <p>Backend Developer Bootcamp</p>
+                            <p>Fullstack Developer Bootcamp</p>
+                        </div>
+                        <hr className='mx-auto'/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Programs
